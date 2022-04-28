3bedac9b41a3390d9365a6490a4336e64e379c4c
marycpriyanka <marycpriyanka@gmail.com>
2022-04-28 13:05:09 -0700

    Add scripts in package.json

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Express middleware
+
+// Static route to serve up the content of our built webpack bundle which is located in the dist folder
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
