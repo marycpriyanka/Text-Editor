534eb6a30b456142628a824662493957c5fee544
marycpriyanka <marycpriyanka@gmail.com>
2022-04-30 13:18:52 -0700

    Add and configure workbox plugins for a service worker and manifest file.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add CSS loaders and babel to webpack.----
 
 module.exports = () => {
   return {
@@ -19,8 +19,40 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    
+    // Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
+     // Webpack plugin that generates our html file and injects our bundles. 
+     new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor'
+    }),
+   
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Text Editor',
+      short_name: 'Editor',
+      description: 'Just another text editor!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
